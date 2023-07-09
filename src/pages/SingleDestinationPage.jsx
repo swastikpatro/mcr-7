33fcef20ec3f74772128775b24c6983c763421ee
swastikpatro <patroswastik.jsdev@gmail.com>
2023-07-09 22:13:37 +0530

    singleDEstinationPage added

--- src/pages/SingleDestinationPage.jsx
+++ src/pages/SingleDestinationPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image } from '@chakra-ui/react';
+import { Box, Heading, Image, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { sectionCenterStyles } from '../GlobalStyles';
@@ -23,16 +23,70 @@ const SingleDestinationPage = () => {
     return <Box>Location not Found</Box>;
   }
 
-  const { id, name, description, image } = destination;
+  const {
+    id,
+    name,
+    description,
+    image,
+    ratings,
+    reviews,
+    ticketPrice,
+    location,
+    openingHours,
+  } = destination;
 
   return (
     <Box sx={sectionCenterStyles}>
       <Heading textAlign={'center'} mb="2rem">
         {name}
       </Heading>
-      <Box display={'grid'} gridTemplateColumns={'300px 1fr'} h="100dvh">
+      <Box
+        display={'grid'}
+        gridTemplateColumns={'300px 1fr'}
+        h="100dvh"
+        gap="2rem"
+      >
         <Box>
           <Image src={image} alt={name} />
+        </Box>
+
+        <Box display={'flex'} flexDir={'column'} gap={'1rem'}>
+          <Text>
+            <Text color="blue.500" as="span">
+              Description:
+            </Text>{' '}
+            {description}
+          </Text>
+          <Text>
+            <Text color="blue.500" as="span">
+              Reviews:
+            </Text>{' '}
+            {reviews}
+          </Text>
+          <Text>
+            <Text color="blue.500" as="span">
+              Rating:
+            </Text>{' '}
+            {ratings}
+          </Text>
+          <Text>
+            <Text color="blue.500" as="span">
+              Ticket Price:
+            </Text>{' '}
+            {ticketPrice}
+          </Text>
+          <Text>
+            <Text color="blue.500" as="span">
+              Location:
+            </Text>{' '}
+            {location}
+          </Text>
+          <Text>
+            <Text color="blue.500" as="span">
+              Opening Hours:
+            </Text>{' '}
+            {openingHours}
+          </Text>
         </Box>
       </Box>
     </Box>
